63f7683e571ea2c87b5ffad63e3ff1b5fd2b6f2c
henriquemarques610 <henriquemarques610@gmail.com>
2021-06-22 19:25:26 +0100

    porjeto be

--- routes/person.js
+++ routes/person.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var personController = require('../controllers/personController')
 
+/* GET users listing. */
 router.get('/', personController.getPersons);
 
 router.post('/', personController.postPersons);
